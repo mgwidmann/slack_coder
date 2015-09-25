42b23b788c74617ae7507472e081dfd3a35fa90b
Matt Widmann <mgwidmann@gmail.com>
2015-09-25 16:06:03 -0400

    Enable favicon as a static asset.

--- lib/slack_coder/endpoint.ex
+++ lib/slack_coder/endpoint.ex
@@ -9,7 +9,7 @@ defmodule SlackCoder.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :slack_coder, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
