c862dcc2da5e6ad9fef0c9b1e7df0877b9779b13
Matt Widmann <mgwidmann@gmail.com>
2015-10-28 13:58:08 -0400

    Add system port config that was accidentally removed.

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,12 @@ use Mix.Config
 
 config :slack_coder, SlackCoder.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
+  http: [port: {:system, "PORT"}],
+  url: [host: "slack-coder.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json"
+
+# Do not print debug messages in production
+config :logger, level: :info
 
 config :slack_coder, SlackCoder.Repo,
   adapter: Ecto.Adapters.Postgres,
