84c158f34b233ea025be0465006b9fcb77b73ace
Matt Widmann <mgwidmann@gmail.com>
2016-01-14 23:06:18 -0500

    Update elixir and force rebuild.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SlackCoder.Mixfile do
   def project do
     [app: :slack_coder,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
