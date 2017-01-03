6d36b7dc1070f38b73f808569f8ca46527b0a06c
Matt Widmann <mgwidmann@gmail.com>
2017-01-03 16:41:47 -0500

    Update flames

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule SlackCoder.Mixfile do
       {:ecto, "2.0.0", override: true},
       {:oauth2, "~> 0.6"},
       {:beaker, github: "hahuang65/beaker"},
-      {:flames, "~> 0.2.0"},
+      {:flames, "~> 0.2.2"},
       {:stub_alias, github: "mgwidmann/stub_alias"},
 
       # Dev only
