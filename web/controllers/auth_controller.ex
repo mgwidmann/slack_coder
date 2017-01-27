733086927f90d97791719cf4400745d26c4399bb
Mikael Davis <mdavis@weddingwire.com>
2017-01-27 14:22:43 -0500

    move find_or_create_user from github lib to user service

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,6 +1,8 @@
 defmodule SlackCoder.AuthController do
   use SlackCoder.Web, :controller
 
+  alias SlackCoder.Services.UserService
+
   @doc """
   This action is reached via `/auth/:provider` and redirects to the OAuth2 provider
   based on the chosen strategy.
@@ -30,7 +32,7 @@ defmodule SlackCoder.AuthController do
 
   def callback(%{assigns: %{ueberauth_auth: %Ueberauth.Auth{extra:
       %Ueberauth.Auth.Extra{raw_info: %{token: %{access_token: token}, user: raw_user}}}}} = conn, _params) do
-    case raw_user |> find_or_create_user() do
+    case raw_user |> UserService.find_or_create_user() do
       {:ok, db_user} ->
         conn
         |> after_login(db_user)
@@ -43,19 +45,6 @@ defmodule SlackCoder.AuthController do
   end
 
   defp authorize_url!, do: SlackCoder.OAuth.Github.authorize_url!
-
-  defp update_user(user, db_user) do
-    User.changeset(db_user || %User{}, user) |> Repo.insert_or_update!
-  end
-
-  defp find_or_create_user(raw_user) do
-    %{github: github} = simplified_user = %{name: raw_user["name"], github: raw_user["login"], slack: raw_user["login"], avatar_url: raw_user["avatar_url"], html_url: raw_user["html_url"], config: %{}}
-    db_user = from(u in User, where: u.github == ^github) |> Repo.one
-
-    user = simplified_user |> update_user(db_user)
-    SlackCoder.Users.Supervisor.start_user(user)
-    if db_user, do: {:ok, user}, else: {:new, user}
-  end
 
   defp after_login(conn, user) do
     conn
