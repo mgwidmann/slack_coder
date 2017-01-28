0baa2629838e39abd01b024a8f1b2794b57fab04
Matt Widmann <mgwidmann@gmail.com>
2017-01-27 23:28:15 -0500

    Removing secret config.

--- config/prod.exs
+++ config/prod.exs
@@ -15,6 +15,23 @@ config :slack_coder, SlackCoder.Repo,
   url: System.get_env("DATABASE_URL"),
   size: 20
 
-if File.exists? "config/prod.secret.exs" do
-  import_config "prod.secret.exs"
-end
+config :slack_coder,
+  slack_api_token: System.get_env("SLACK_API_TOKEN"),
+  personal: true,
+  timezone: "America/New_York",
+  pr_backoff_start: 4,
+  caretaker: :matt
+
+config :slack_coder, :github,
+  pat: System.get_env("GITHUB_PAT"),
+  user: System.get_env("GITHUB_USER")
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
+
+config :slack_coder, :notifications,
+  min_hour: 8,
+  max_hour: 17,
+  always_allow: false,
+  days: [1,2,3,4,5]
