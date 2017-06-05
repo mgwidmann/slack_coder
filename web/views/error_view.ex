6df952f86c581c050b6e3aede15b5c0ac14922ad
Matt Widmann <mgwidmann@gmail.com>
2017-06-05 14:25:06 -0400

    Add json responses.

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -9,9 +9,17 @@ defmodule SlackCoder.ErrorView do
     {:safe, File.read!("priv/static/500.html")}
   end
 
+  def render("404.json", _assigns) do
+    %{status: 404, message: "Not found"}
+  end
+
+  def render("500.json", _assigns) do
+    %{status: 500, message: "Internal Server Error"}
+  end
+
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
-    render "500.html", assigns
+    render "500", assigns
   end
 end
