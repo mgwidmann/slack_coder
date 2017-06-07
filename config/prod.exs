ab79c663311913497035d39adf2990c0ac3cb739
Matt Widmann <mgwidmann@gmail.com>
2017-06-07 10:43:09 -0400

    Change name of manifest file.

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :slack_coder, SlackCoder.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   http: [port: {:system, "PORT"}],
   url: [host: "slack-coder.nanoapp.io", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger,
   # Do not print debug messages in production
