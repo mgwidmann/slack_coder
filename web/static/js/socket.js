f19c052d58e69933a703441eafa604daec1b6d63
Matt Widmann <mgwidmann@gmail.com>
2017-06-07 00:04:55 -0400

    Fix brunch build.

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -3,7 +3,7 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
-import {Socket} from "deps/phoenix/web/static/js/phoenix"
+import {Socket} from "../../../deps/phoenix/assets/js/phoenix";
 
 let socket = new Socket("/socket")
 
