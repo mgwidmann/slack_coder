8ffb2868d5475ee62853b01dca592e726cdac036
Matt Widmann <mgwidmann@gmail.com>
2017-06-06 23:57:23 -0400

    Unused variable.

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -16,7 +16,7 @@ defmodule SlackCoder.ErrorView do
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
-  def template_not_found(template, assigns) do
+  def template_not_found(_template, assigns) do
     render "500.html", assigns
   end
 end
