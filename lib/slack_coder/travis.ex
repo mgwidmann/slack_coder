131d13dbc22ba33523821c771c6cb328e4e00382
Matt Widmann <mgwidmann@gmail.com>
2017-09-11 11:09:22 -0400

    Make travis error handling a bit more robust and log warnings instead of errors.

--- lib/slack_coder/travis.ex
+++ lib/slack_coder/travis.ex
@@ -5,28 +5,39 @@ defmodule SlackCoder.Travis do
 
   def build_info(owner, repo, build) do
     "/repos/#{owner}/#{repo}/builds/#{build}"
-    |> Build.get!()
+    |> Build.get()
+    |> case do
+      {:ok, response} ->
+        response
+      {:error, problem} ->
+        Logger.warn """
+        #{__MODULE__} job build fetch failed
+
+        #{inspect problem}
+        """
+        [] # Return nothing
+    end
     |> Map.fetch!(:body)
   end
 
   def job_log(build) do
     "/jobs/#{build.id}/log"
-    |> Job.get!()
+    |> Job.get()
     |> case do
-      %HTTPoison.Response{status_code: 200, body: body} ->
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         body
         |> Job.filter_log()
         |> Job.new()
       # Cannot use follow_redirect: true, cause need to drop authorization header
-      %HTTPoison.Response{status_code: 307, headers: headers} ->
+      {:ok, %HTTPoison.Response{status_code: 307, headers: headers}} ->
         {"Location", redirect} = Enum.find(headers, &(match?({"Location", _}, &1)))
         HTTPoison.get!(redirect)
         |> Map.fetch!(:body)
         |> Job.filter_log()
         |> Job.new()
-      %HTTPoison.Response{status_code: code} = response ->
-        Logger.error """
-        #{__MODULE__} job log fetch failed with HTTP status code: #{code}
+      {ok_or_error, response} when ok_or_error in [:ok, :error] ->
+        Logger.warn """
+        #{__MODULE__} job log fetch failed
 
         #{inspect response}
         """
