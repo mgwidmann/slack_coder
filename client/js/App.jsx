2735a0f7f8b070763951576927d2af906e9f1369
Matt Widmann <mgwidmann@gmail.com>
2017-10-04 21:59:59 -0400

    Upgrade react to use error boundaries and add one to catch any errors.

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 import { store, history } from './app/store';
 import client from './app/client';
 import Routing from './Routing';
+import ErrorBoundary from './app/containers/ErrorBoundary';
 import '../css/app.scss';
 import 'bootstrap';
 import 'react-select/dist/react-select.css';
@@ -13,7 +14,9 @@ const App = () => {
   return (
     <ApolloProvider client={client} store={store}>
       <ConnectedRouter dispatch={store.dispatch} history={history}>
-        <Routing store={store}/>
+        <ErrorBoundary>
+          <Routing store={store}/>
+        </ErrorBoundary>
       </ConnectedRouter>
     </ApolloProvider>
   );
