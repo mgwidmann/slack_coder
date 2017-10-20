a4f42c90957b894bc799dc65f4b41d062780625d
Matt Widmann <mgwidmann@gmail.com>
2017-10-20 00:12:13 -0400

    Introduce environment based configuration

--- client/mobile/app/client.js
+++ client/mobile/app/client.js
@@ -1,5 +1,6 @@
 import configureClient from '../shared/graphql/client';
 import websocketNetworkInterface from '../shared/graphql/websocketNetworkInterface';
 import createSocket from './socket';
+import Config from 'react-native-config';
 
-export default configureClient(websocketNetworkInterface('ws://slack-coder.dev:4000/socket', createSocket));
+export default configureClient(websocketNetworkInterface(`ws://${Config.SLACK_CODER_HOST}/socket`, createSocket));
