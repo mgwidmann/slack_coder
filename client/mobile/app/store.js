225e2cce252b8237cf2d817b7b993e0fe25350dd
Matt Widmann <mgwidmann@gmail.com>
2017-10-24 22:51:57 -0400

    Ensure logger doesnt show up in production

--- client/mobile/app/store.js
+++ client/mobile/app/store.js
@@ -4,15 +4,19 @@ import { createLogger } from 'redux-logger';
 import client from './client';
 import reducers from './reducers';
 
-// const loggerMiddleware = createLogger({ predicate: (getState, action) => true, collapsed: true });
+let middleware = [
+  thunkMiddleware,
+  client.middleware()
+]
+
+if (__DEV__) {
+  const loggerMiddleware = createLogger({ predicate: (getState, action) => true, collapsed: true });
+  middleware.push(loggerMiddleware);
+}
 
 function configureStore(initialState) {
   const enhancer = compose(
-    applyMiddleware(
-      thunkMiddleware,
-      // loggerMiddleware,
-      client.middleware()
-    ),
+    applyMiddleware(...middleware),
     (typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined') ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   );
   return createStore(reducers, initialState, enhancer);
